ae1547835c927e7802e843387fe8a796ea98d74a
Alexander Sencion Cespedes <alexandersenc99@gmail.com>
2025-11-13 04:58:19 -0400

    Add Product + Category Entities

---
+++ Models/Product.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace ProductCatalogApi.Models;
+
+public class Product
+{
+    public int Id { get; set; }
+    public string Name { get; set; } = string.Empty;
+    public decimal Price { get; set; }
+    public double Rating { get; set; }
+    public bool IsActive { get; set; }
+    public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+    public int CategoryId { get; set; }
+    public Category? Category { get; set; }
+}
