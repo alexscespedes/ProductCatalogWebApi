c83fba4280623c119f266a3651d57dc2fbd4d664
Alexander Sencion Cespedes <alexandersenc99@gmail.com>
2025-11-18 05:04:24 -0400

    Create the Controller with GetProducts

---
+++ Controllers/ProductsController.cs
@@ -0,0 +1,26 @@
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using ProductCatalogApi.DTOs;
+using ProductCatalogApi.Service;
+
+namespace ProductCatalogApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        private readonly ProductService _service;
+
+        public ProductsController(ProductService service)
+        {
+            _service = service;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetProducts([FromQuery] ProductQueryParameters parameters)
+        {
+            var result = await _service.GetProductsAsync(parameters);
+            return Ok(result);
+        }
+    }
+}
